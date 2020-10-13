174b34d0f709cf9598e2721af5cf505736b41f2c
FahimFaq <57095847+FahimFaq@users.noreply.github.com>
2020-10-13 13:52:50 +0200

    added the api fetch with axios then checked the lifecycle and shared the props

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
 import React from "react";
-// import Cards from "./components/Cards/Cards";
-// import Chart from "./components/Chart/Chart";
-// import CountryPicker from "./components/CountryPicker/CountryPickers";
 
+// import components
 import { Cards, Chart, CountryPicker } from "./components";
 import styles from "./App.module.css";
+import { fetchData } from "./api";
 
 class App extends React.Component {
+  // set the fetched data in the state
+  state = {
+    // create an object called data thats unpopulated
+    data: {},
+  };
+
+  /* create a async lifecycle component called componentDidMount and check
+  the if the data rendered */
+  async componentDidMount() {
+    // create a variable called data and await the fetched reponse
+    const fetchedData = await fetchData();
+
+    /* populate the data object in the state with setState, then create
+    a data property equal to the fetchData variable */
+    this.setState({ data: fetchedData });
+  }
   render() {
+    // create a variable and  deconstruct the data from the state
+    const { data } = this.state;
     return (
       <div className={styles.container}>
-        <Cards />
+        {/* add data props to card component */}
+        <Cards data={data} />
         <CountryPicker />
         <Chart />
       </div>
---
+++ src/api/index.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+// create a variabel called url and add the API url
+const url = "https://covid19.mathdro.id/api";
+
+// create a async arrow function called fetchData to fetch the data from the API
+export const fetchData = async () => {
+  // create a try and catch block to catch any errors
+  try {
+    /* executed if fetch is succesful then create a variable called 
+    response and get the response from the await then get the url with 
+    axios then destructure the data from the response */
+    const {
+      data: { confirmed, recovered, deaths, lastUpdate },
+    } = await axios.get(url);
+
+    /* return the data properties in a simple return statement, 
+    we do not need to store it in an object because we return it right after */
+    return { confirmed, recovered, deaths, lastUpdate };
+  } catch (error) {
+    // executed if fetch is unsuccesfull
+  }
+};
